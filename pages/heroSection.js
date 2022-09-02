936923158341218b2d243c412d068a98bd7d7f4f
Georgiy S <draw2know@icloud.com>
2022-09-02 18:07:56 +0200

    fixes: minor bugs

--- pages/heroSection.js
+++ pages/heroSection.js
@@ -2,7 +2,7 @@ import styles from "@/styles/heroSection.module.scss";
 
 export default function HeroSection() {
   return (
-    <div id="heroSection" className={styles.mainContainer} id="heroSection">
+    <div id="heroSection" className={styles.mainContainer}>
       <h3>... comming schuuun! ...</h3>
     </div>
   );
--- pages/impressum.js
+++ pages/impressum.js
@@ -1,6 +1,6 @@
 import style from "@/styles/impressum.module.scss";
 
-export default function () {
+export default function Impressum() {
   return (
     <div className={style.mainContainer}>
       <h1>Impressum</h1>
