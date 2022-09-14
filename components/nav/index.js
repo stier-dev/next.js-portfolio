3a4a4a2090b33527fd34de6ffd6b56334d76e202
Georgiy S <draw2know@icloud.com>
2022-09-14 09:01:56 +0200

    testing: run build: nav error_2

--- components/nav/index.js
+++ components/nav/index.js
@@ -12,41 +12,34 @@ export default function Nav() {
   const router = useRouter();
 
   return (
-    // <div className={style.navbar}>
-    //   <div className={style.menues}>
-    //     <div className={style.mainBackground} />
-    //     {/* Startseite Logo */}
-    //     <div
-    //       className={style.homeImgContainer}
-    //       onMouseEnter={() => setHoverLogo(true)}
-    //       onMouseLeave={() => setHoverLogo(false)}
-    //       onClick={() => {
-    //         router.push("/");
-    //       }}
-    //     >
-    //       <Image
-    //         layout="fill"
-    //         src={
-    //           !hoverLogo ? "/img/navbar/logo.svg" : "/img/navbar/logo_hover.svg"
-    //         }
-    //       />
-    //     </div>
-    //     <div
-    //       onClick={() => {
-    //         setNavActive(!navActive);
-    //       }}
-    //     >
-    //       <HamburgerBtn state={navActive} />
-    //     </div>
-    //     <MobileMenue state={navActive} />
-    //     <DesktopMenue />
-    
-        {/* Main Menue */}
-        {/* <div
-          className={`${style.mobileBackground}  ${
-            navActive ? style.openMobileBackground : ""
-          }`}
-        /> */}
+    <div className={style.navbar}>
+      <div className={style.menues}>
+        <div className={style.mainBackground} />
+        {/* Startseite Logo */}
+        <div
+          className={style.homeImgContainer}
+          onMouseEnter={() => setHoverLogo(true)}
+          onMouseLeave={() => setHoverLogo(false)}
+          onClick={() => {
+            router.push("/");
+          }}
+        >
+          <Image
+            layout="fill"
+            src={
+              !hoverLogo ? "/img/navbar/logo.svg" : "/img/navbar/logo_hover.svg"
+            }
+          />
+        </div>
+        <div
+          onClick={() => {
+            setNavActive(!navActive);
+          }}
+        >
+          <HamburgerBtn state={navActive} />
+        </div>
+        <MobileMenue state={navActive} />
+        <DesktopMenue />
       </div>
     </div>
   );
