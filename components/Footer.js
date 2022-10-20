ae4e16be6f5f92a40668030d2986acfdb7313b6e
Georgiy S <draw2know@icloud.com>
2022-10-20 13:22:15 +0200

    added react-scroll-parallax

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,21 @@
 import styles from "@/styles/components/Footer.module.scss";
 import Link from "next/link";
 import style from "@/styles/components/Footer.module.scss";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 export default function Footer() {
   return (
-    <div className={styles.footerContainer}>
-      <div className={style.linksContainer}>
-        <Link href="/impressum">
-          <h5 className={style.link}>Impressum</h5>
-        </Link>
-        <Link href="/datenschutz">
-          <h5 className={style.link}>Datenschutz</h5>
-        </Link>
+    <ParallaxProvider>
+      <div className={styles.footerContainer}>
+        <div className={style.linksContainer}>
+          <Link href="/impressum">
+            <h5 className={style.link}>Impressum</h5>
+          </Link>
+          <Link href="/datenschutz">
+            <h5 className={style.link}>Datenschutz</h5>
+          </Link>
+        </div>
       </div>
-    </div>
+    </ParallaxProvider>
   );
 }
