a0585c3f1ef7587a466acda5452dff348dbfdbff
Georgiy S <draw2know@icloud.com>
2023-03-12 10:10:32 +0100

    nav: FIXED scrolling to section by making placeholder for not yet loaded section the same size as the loaded section

--- pages/kaleidoskop.js
+++ pages/kaleidoskop.js
@@ -2,7 +2,7 @@ import style from "@/styles/kaleidoskop.module.scss";
 
 export default function HeroSection() {
   return (
-    <div id="kaleidoskop" className={style.mainContainer}>
+    <div className={style.mainContainer}>
       <iframe
         src="/iframe/kaleidoskop/kaleidoskop.html"
         width="100%"
